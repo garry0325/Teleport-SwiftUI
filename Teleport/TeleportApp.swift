90f8a19e10b69e2a98819a943ccda5b1c762508d
garry0325 <garry0325@gmail.com>
2024-09-06 22:30:50 -0700

    Initial Commit

---
+++ Teleport/TeleportApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleportApp.swift
+//  Teleport
+//
+//  Created by Garry Sinica on 2024/9/6.
+//
+
+import SwiftUI
+
+@main
+struct TeleportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
